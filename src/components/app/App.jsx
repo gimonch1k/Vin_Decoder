c9794d3954e918211c8c145653993eadc23c32ba
Dmytro <dimaplyakin2007@gmail.com>
2026-07-27 20:17:24 +0300

    initial commit

---
+++ src/components/app/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+
+import "./app.scss";
+
+function App() {
+  return (
+    <Router>
+      <div className="app"></div>
+    </Router>
+  );
+}
+
+export default App;
