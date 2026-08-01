1fb08eaa84e0f4521778bf6638de384db91da40d
Dmytro <dimaplyakin2007@gmail.com>
2026-08-01 22:49:32 +0300

    modified Page404

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router";
+
 function Page404() {
-  return <h2>404</h2>;
+  return (
+    <>
+      <h2 style={{ textAlign: "center" }}>404</h2>
+      <Link
+        to="/"
+        style={{
+          fontSize: "18px",
+          fontWeight: "700",
+          margin: "30px auto 0 auto",
+          display: "block",
+          width: "fit-content",
+        }}
+      >
+        Повернутись на головну
+      </Link>
+    </>
+  );
 }
 
 export default Page404;
